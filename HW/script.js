65ea68e3c2377a6b3cf2cd1e7ddff0832433372f
zvolaja <zvolaja@student.uzlabina.cz>
2022-10-26 10:29:04 +0200

    new

--- HW/script.js
+++ HW/script.js
@@ -1,5 +1,10 @@
-setInterval(()=>{
-        document.getElementById("cont").style.marginRight = (screen.width/90)*(screen.width/90) + "px";
-        document.getElementById("cont").style.marginLeft = (screen.width/90)*(screen.width/90) + "px";
-        console.log(document.getElementById("cont").style.marginRight);
+setInterval(() => {
+    document.getElementById("cont").style.marginRight = (document.getElementById("container").scrollWidth / 90) * (document.getElementById("container").scrollWidth / 90) + "px";
+    document.getElementById("cont").style.marginLeft = (document.getElementById("container").scrollWidth / 90) * (document.getElementById("container").scrollWidth / 90) + "px";
+    //console.log(document.getElementById("cont").style.marginRight + "   " + document.getElementById("cont").style.marginLeft);
+    //if (screen.height < 605) {
+    document.getElementById("li").style.paddingBottom = (document.body.scrollHeight/2 - 182) + "px";
+    document.getElementById("li").style.paddingTop = (document.body.scrollHeight/2 - 182) + "px";
+    console.log(document.getElementById("li").style.paddingBottom + "    " + document.getElementById("li").style.paddingTop);
+    //}
 }, 1);
