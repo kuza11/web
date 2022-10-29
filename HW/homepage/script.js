bf00d570852669ef19abc918764cca5a4c329147
kuza11 <kuba.zvolanek@seznam.cz>
2022-10-29 08:40:12 +0200

    folders

---
+++ HW/homepage/script.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+    document.getElementById("cont").style.marginRight = (document.getElementById("container").scrollWidth / 86) * (document.getElementById("container").scrollWidth / 86) + "px";
+    document.getElementById("cont").style.marginLeft = (document.getElementById("container").scrollWidth / 86) * (document.getElementById("container").scrollWidth / 86) + "px";
+    //console.log(document.getElementById("cont").style.marginRight + "   " + document.getElementById("cont").style.marginLeft);
+    //if (screen.height < 605) {
+    document.getElementById("li").style.paddingBottom = (document.body.scrollHeight/2 - 182) + "px";
+    document.getElementById("li").style.paddingTop = (document.body.scrollHeight/2 - 182) + "px";
+    console.log(document.getElementById("li").style.paddingBottom + "    " + document.getElementById("li").style.paddingTop);
+    //}
+}, 1);
