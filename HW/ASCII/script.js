5884aa433b65d1d213b41623b688ba191b3436d6
kuza11 <kuba.zvolanek@seznam.cz>
2022-10-30 08:37:19 +0100

    js

--- HW/ASCII/script.js
+++ HW/ASCII/script.js
@@ -1,8 +1,33 @@
+let switched = false
+
 setInterval(() => {
     document.getElementById("cont").style.marginRight = (document.getElementById("container").scrollWidth / 86) * (document.getElementById("container").scrollWidth / 86) + "px";
     document.getElementById("cont").style.marginLeft = (document.getElementById("container").scrollWidth / 86) * (document.getElementById("container").scrollWidth / 86) + "px";
 
-    document.getElementById("li").style.paddingBottom = (document.body.scrollHeight/2 - 182) + "px";
-    document.getElementById("li").style.paddingTop = (document.body.scrollHeight/2 - 182) + "px";
+    document.getElementById("li").style.paddingBottom = (document.body.scrollHeight / 2 - 182) + "px";
+    document.getElementById("li").style.paddingTop = (document.body.scrollHeight / 2 - 182) + "px";
 
 }, 1);
+
+function conversion() {
+    document.getElementById("CIco").src = "../convert-icon-dark.png"
+    document.getElementById("outputT").innerHTML = "";
+    if (!switched) {
+        document.getElementById("textA").value.split(" ").forEach(e => {
+            try {
+                e.split("").forEach(num => {
+                    if (Number.isNaN(parseInt(num))) throw "NaN";
+                });
+                e = parseInt(e);
+                if (Number.isNaN(e)) throw "NaN";
+                if (document.getElementById("outputT").innerHTML != "Invalid input!")
+                    document.getElementById("outputT").innerHTML += " " + String(e);
+            } catch (error) {
+                return document.getElementById("outputT").innerHTML = "Invalid input!";
+            }
+        });
+    }
+    setTimeout(() => {
+        document.getElementById("CIco").src = "../convert-icon.png"
+    }, 100);
+}
