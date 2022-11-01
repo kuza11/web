129ebfe35b2c45b99eaee2c5d344ebdcbd0650c5
zvolaja <zvolaja@student.uzlabina.cz>
2022-11-01 08:07:33 +0100

    footer fix

--- HW/UTF-16/script.js
+++ HW/UTF-16/script.js
@@ -4,8 +4,8 @@ setInterval(() => {
     document.getElementById("cont").style.marginRight = (document.getElementById("container").scrollWidth / 86) * (document.getElementById("container").scrollWidth / 86) + "px";
     document.getElementById("cont").style.marginLeft = (document.getElementById("container").scrollWidth / 86) * (document.getElementById("container").scrollWidth / 86) + "px";
 
-    document.getElementById("li").style.paddingBottom = (document.body.scrollHeight / 2 - 182) + "px";
-    document.getElementById("li").style.paddingTop = (document.body.scrollHeight / 2 - 182) + "px";
+    document.getElementById("li").style.paddingBottom = (document.body.scrollHeight / 2 - 166) + "px";
+    document.getElementById("li").style.paddingTop = (document.body.scrollHeight / 2 - 166) + "px";
 
 }, 1);
 
